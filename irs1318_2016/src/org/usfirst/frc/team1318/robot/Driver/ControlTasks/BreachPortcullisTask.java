9d87c0bf24468d936a5dd934c68af3ff5e518545
RadiantCorrin <corbmod@gmail.com>
2016-02-04 19:51:41 -0800

    Cleaned up some comments and formatting in the BreachPortcullisTask.java

--- irs1318_2016/src/org/usfirst/frc/team1318/robot/Driver/ControlTasks/BreachPortcullisTask.java
+++ irs1318_2016/src/org/usfirst/frc/team1318/robot/Driver/ControlTasks/BreachPortcullisTask.java
@@ -5,6 +5,11 @@ import org.usfirst.frc.team1318.robot.TuningConstants;
 import org.usfirst.frc.team1318.robot.DriveTrain.DriveTrainComponent;
 import org.usfirst.frc.team1318.robot.Driver.Operation;
 
+/**
+ * @author Preston and Corbin
+ * This class is a task designed to automatically drive the robot through the portcullis.
+ */
+
 public class BreachPortcullisTask extends ControlTaskBase
 {
     private double startDTDistanceRight;
@@ -22,19 +27,21 @@ public class BreachPortcullisTask extends ControlTaskBase
     {
         driveTrain = getComponents().getDriveTrain();
 
+        // Log the starting distance of the encoders (for the drive train)
         startDTDistanceRight = driveTrain.getRightEncoderDistance();
         startDTDistanceLeft = driveTrain.getLeftEncoderDistance();
 
+        // Set the desired position for the drive train
         desiredDTDistanceRight = startDTDistanceRight + TuningConstants.PORTCULLIS_BREACH_DISTANCE;
         desiredDTDistanceLeft = startDTDistanceLeft + TuningConstants.PORTCULLIS_BREACH_DISTANCE;
-
     }
 
     @Override
     public void begin()
     {
-        // TODO Auto-generated method stub
+        // Reset the defense arm
         setDigitalOperationState(Operation.DefenseArmMoveToFront, true);
+        // Set necessary operations to true
         setDigitalOperationState(Operation.DriveTrainUsePositionalMode, true);
         setDigitalOperationState(Operation.DefenseArmUsePositionalMode, true);
     }
@@ -62,13 +69,12 @@ public class BreachPortcullisTask extends ControlTaskBase
         
         // Set the desired arm angle converted to ticks
         setAnalogOperationState(Operation.DefenseArmSetAngle, armAngle * TuningConstants.DEFENSE_ARM_RADIANS_TO_TICKS);
-
     }
 
     @Override
     public void stop()
     {
-        // TODO Auto-generated method stub
+        // Disable positional modes for drive train and defense arm
         setDigitalOperationState(Operation.DriveTrainUsePositionalMode, false);
         setDigitalOperationState(Operation.DefenseArmUsePositionalMode, false);
     }
@@ -76,16 +82,17 @@ public class BreachPortcullisTask extends ControlTaskBase
     @Override
     public void end()
     {
-        // TODO Auto-generated method stub
+        // Disable positional modes for drive train and defense arm
         setDigitalOperationState(Operation.DriveTrainUsePositionalMode, true);
         setDigitalOperationState(Operation.DefenseArmUsePositionalMode, true);
+        // Return defense arm to the front of the robot
         setDigitalOperationState(Operation.DefenseArmMoveToFront, true);
     }
 
     @Override
     public boolean hasCompleted()
     {
-        // TODO Auto-generated method stub
+        // Check that the distance the robot has traveled (with the purpose of returning true if the desired position has been met)
         if (currentDTDistanceRight >= desiredDTDistanceRight && currentDTDistanceLeft >= desiredDTDistanceLeft)
         {
             return true;
