6b83f317859bc1ad0bbfed7722c1cc6fce0b4a58
WRall <wm_rall@hotmail.com>
2016-05-17 20:00:41 -0700

    Removing 2016-specific tuning constants

--- irs1318_2016/src/org/usfirst/frc/team1318/robot/TuningConstants.java
+++ irs1318_2016/src/org/usfirst/frc/team1318/robot/TuningConstants.java
@@ -60,77 +60,7 @@ public class TuningConstants
     public static final double DRIVETRAIN_REVERSE_RIGHT_SCALE_FACTOR = 1.15;//moving forwards
     public static final double DRIVETRAIN_REVERSE_LEFT_SCALE_FACTOR = 1.17;//moving backwards
 
-    //================================================== Shooter ==============================================================
-
-    public static final boolean SHOOTER_SCALE_BASED_ON_VOLTAGE = false;
-    public static final double SHOOTER_VELOCITY_TUNING_VOLTAGE = 12.5;
-
-    public static final double SHOOTER_MAX_POWER_LEVEL = 1.0;
-
-    public static final double SHOOTER_VELOCITY_PID_KP_DEFAULT = 0.1;
-    public static final double SHOOTER_VELOCITY_PID_KI_DEFAULT = 0.0;
-    public static final double SHOOTER_VELOCITY_PID_KD_DEFAULT = 0.0;
-    public static final double SHOOTER_VELOCITY_PID_KF_DEFAULT = 1.0;
-    public static final double SHOOTER_VELOCITY_PID_KS_DEFAULT = 40.0;
-
-    public static final double SHOOTER_CLOSE_SHOT_VELOCITY = 0.7;
-    public static final double SHOOTER_MIDDLE_SHOT_VELOCITY = 0.52;
-    public static final double SHOOTER_FAR_SHOT_VELOCITY = 0.7;
-   
-    public static final double SHOOTER_LOWER_KICKER_DURATION = 0.5;
-    public static final double SHOOTER_SPIN_UP_DURATION = 1.75;
-    public static final double SHOOTER_FIRE_DURATION = 0.75;
-
-    public static final double SHOOTER_REVERSE_DURATION = 0.5;
-    
-    public static final double SHOOTER_DEVIANCE = 0.025;
-    public static final double SHOOTER_MAX_COUNTER_RATE = 2000;
-    
-    public static final double SHOOTER_TARGETING_LIGHT_ACTIVATION_THRESHOLD = 0.5;
-
-    //================================================== Intake ==============================================================
-
-    public static final double INTAKE_IN_POWER_LEVEL = 0.9;
-    public static final double INTAKE_OUT_POWER_LEVEL = -0.9;
-
-    //================================================== Climber ==============================================================
-
-    public static final double CLIMBER_WINCH_MAX_SPEED = 0.8;
-    public static final double CLIMBER_HOOK_MAX_SPEED = 0.8;
-    public static final double CLIMBER_ENCODER_DISTANCE_PER_PULSE = 1.0;
-    public static final double CLIMBER_ACCEPTABLE_DELTA = 0.025;
-    public static final double CLIMBER_FIRE_DURATION = 0.5;
-    public static final double CLIMBER_CLIMB_DURATION = 9.0;
-    public static final double CLIMBER_CLIMB_DISTANCE = 1000.0;
-
-    //================================================== Stinger ==============================================================
-
-    public static final double STINGER_MAX_VELOCTIY = 0.5;
-    public static final double STINGER_SLOW_BACK_VELOCTIY = -0.275;
-
-    //================================================== Breach Macros ==============================================================
-
-    public static final double START_TO_PORTCULLIS_DISTANCE = 24.0 * 2.54;//74.0 * 2.54;
-    public static final double START_TO_CHEVAL_DE_FRISE_DISTANCE = 24.0 * 2.54;//74.0 * 2.54;
-
     //================================================== Autonomous ==============================================================
 
     public static final double DRIVETRAIN_POSITIONAL_ACCEPTABLE_DELTA = 1.0;
-    
-    // Drivetrain autonomous velocity values
-    public static final double DRIVETRAIN_AUTONOMOUS_SLOW_VELOCITY = .3;
-    public static final double DRIVETRAIN_AUTONOMOUS_FAST_VELOCITY = .45;
-    
-    public static final double AUTONOMOUS_DEFENSE_BREACH_DISTANCE = 20 * 12 * 2.54;// Distance from start through a defense in autonomous
-    public static final double AUTONOMOUS_TIME_SLOW = 4.0;
-    public static final double AUTONOMOUS_TIME_FAST = 3.0;
-    
-    // Cheval De Frise values
-    public static final double AUTONOMOUS_CHEVAL_BREACH_TIME = 3;
-    public static final double AUTONOMOUS_CHEVAL_BREACH_DISTANCE = 500;
-    
-    //Portcullis values
-    public static final double AUTONOMOUS_PORTCULLIS_BREACH_TIME = 3;
-    public static final double AUTONOMOUS_PORTCULLIS_BREACH_DISTANCE = 500;
-
-}
+ }
