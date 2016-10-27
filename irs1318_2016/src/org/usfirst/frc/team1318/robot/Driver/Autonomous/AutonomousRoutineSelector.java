e4eaa159152698c3423a32af4b0c83ad3feed667
WRall <wm_rall@hotmail.com>
2016-10-26 20:33:36 -0700

    Move autonomous routine selection to helper class.

--- irs1318_2016/src/org/usfirst/frc/team1318/robot/Driver/Autonomous/AutonomousRoutineSelector.java
+++ irs1318_2016/src/org/usfirst/frc/team1318/robot/Driver/Autonomous/AutonomousRoutineSelector.java
@@ -1,160 +1,261 @@
- package org.usfirst.frc.team1318.robot;
+package org.usfirst.frc.team1318.robot.Driver.Autonomous;
 
+import org.usfirst.frc.team1318.robot.ElectronicsConstants;
+import org.usfirst.frc.team1318.robot.TuningConstants;
 import org.usfirst.frc.team1318.robot.Common.DashboardLogger;
-import org.usfirst.frc.team1318.robot.Driver.Driver;
-import org.usfirst.frc.team1318.robot.Driver.Autonomous.AutonomousDriver;
-import org.usfirst.frc.team1318.robot.Driver.User.UserDriver;
+import org.usfirst.frc.team1318.robot.Driver.IControlTask;
+import org.usfirst.frc.team1318.robot.Driver.ControlTasks.ConcurrentTask;
+import org.usfirst.frc.team1318.robot.Driver.ControlTasks.DriveRouteTask;
+import org.usfirst.frc.team1318.robot.Driver.ControlTasks.DriveTimedTask;
+import org.usfirst.frc.team1318.robot.Driver.ControlTasks.IntakeExtendTask;
+import org.usfirst.frc.team1318.robot.Driver.ControlTasks.PIDBrakeTask;
+import org.usfirst.frc.team1318.robot.Driver.ControlTasks.SequentialTask;
+import org.usfirst.frc.team1318.robot.Driver.ControlTasks.ShooterKickerTask;
+import org.usfirst.frc.team1318.robot.Driver.ControlTasks.ShooterSpinDownTask;
+import org.usfirst.frc.team1318.robot.Driver.ControlTasks.ShooterSpinUpTask;
+import org.usfirst.frc.team1318.robot.Driver.ControlTasks.StingerTask;
+import org.usfirst.frc.team1318.robot.Driver.ControlTasks.TurnTask;
+import org.usfirst.frc.team1318.robot.Driver.ControlTasks.WaitTask;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.DigitalInput;
 
-/**
- * Main class for the FRC 2016 Stronghold Competition
- * Robot for IRS1318 - Gimli
- * 
- * 
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package, you
- * must also update the manifest file in the resource directory.
- * 
- * 
- * General design comments:
- * We have three types of objects:
- * - Driver - describes the driver/operator of the robot ("autonomous" or "user")
- * - Components - describe the electronics of an mechanism and defines the abstract way to control those electronics.
- * - Controllers - define the logic that controls a mechanism given inputs/outputs.
- * 
- * @author Will
- */
-public class Robot extends IterativeRobot
+public class AutonomousRoutineSelector
 {
-    // smartdash logging constants
-    private static final String ROBOT_STATE_LOG_KEY = "r.s";
+    // smartdash other constants
+    private static final String AUTONOMOUS_ROUTINE_PREFERENCE_KEY = "a.routine value";
 
-    // Driver.  This could either be the UserDriver (joystick) or the AutonomousDriver
-    private Driver driver;
-
-    // Components
-    private ComponentManager components;
-
-    // Controllers
-    private ControllerManager controllers;
+    // DipSwitches for selecting autonomous mode
+    private DigitalInput dipSwitchA;
+    private DigitalInput dipSwitchB;
+    private DigitalInput dipSwitchC;
 
     /**
-     * Robot-wide initialization code should go here.
-     * This default Robot-wide initialization code will be called when 
-     * the robot is first powered on.  It will be called exactly 1 time.
+     * Initializes a new AutonomousDriver
      */
-    public void robotInit()
-    {                               
-        // create mechanism components
-        this.components = new ComponentManager();
+    public AutonomousRoutineSelector()
+    {        
+        this.dipSwitchA = new DigitalInput(ElectronicsConstants.AUTONOMOUS_DIP_SWITCH_A);
+        this.dipSwitchB = new DigitalInput(ElectronicsConstants.AUTONOMOUS_DIP_SWITCH_B);
+        this.dipSwitchC = new DigitalInput(ElectronicsConstants.AUTONOMOUS_DIP_SWITCH_C);
+    }
 
-        // create controllers for each mechanism
-        this.controllers = new ControllerManager(this.components);
+    /**
+     * Check what routine we want to use and return it
+     * @return autonomous routine to execute during autonomous mode
+     */
+    public IControlTask selectRoutine()
+    {
+        int routineSelection = 0;
+        if (!this.dipSwitchA.get())
+        {
+            routineSelection += 1;
+        }
+
+        if (!this.dipSwitchB.get())
+        {
+            routineSelection += 2;
+        }
         
-        DashboardLogger.putString(Robot.ROBOT_STATE_LOG_KEY, "Init");
-    }
-
-    /**
-     * Initialization code for disabled mode should go here.
-     * This code will be called each time the robot enters disabled mode.
-     */
-    public void disabledInit()
-    {
-        if (this.driver != null)
+        if (!this.dipSwitchC.get())
         {
-            this.driver.stop();
+            routineSelection += 4;
         }
 
-        if (this.controllers != null)
+        // print routine selection to the smartdash
+        DashboardLogger.putInteger(AutonomousRoutineSelector.AUTONOMOUS_ROUTINE_PREFERENCE_KEY, routineSelection);
+        
+        // Select autonomous routine based on the dipswitch positions
+        switch (routineSelection)
         {
-            this.controllers.stop();
+            case 0: // No switches flipped
+                return AutonomousRoutineSelector.GetFillerRoutine();
+
+            case 1: // Switch A flipped
+                return AutonomousRoutineSelector.GetDriveTimedRoutine(
+                    TuningConstants.AUTONOMOUS_TIME_SLOW,
+                    0.0,
+                    TuningConstants.DRIVETRAIN_AUTONOMOUS_SLOW_VELOCITY);
+
+            case 2: // Switch B flipped
+                return AutonomousRoutineSelector.GetDriveTimedRoutine(
+                    TuningConstants.AUTONOMOUS_TIME_FAST,
+                    0.0,
+                    TuningConstants.DRIVETRAIN_AUTONOMOUS_FAST_VELOCITY);
+
+            case 3: // Switches A and B flipped
+                return AutonomousRoutineSelector.GetPortcullisBreachRouteRoutine();
+                
+            case 4: // Switch C flipped
+                return AutonomousRoutineSelector.GetDriveStraightAndTurnAndShootCloseRouteRoutine();
+                
+            case 5: // Switches A and C flipped
+                return AutonomousRoutineSelector.GetChevalDeFriseBreachRouteRoutine();
+                
+            case 6: // Switches B and C flipped
+                return AutonomousRoutineSelector.GetDriveStraightAndCurveAndShootCloseRouteRoutine();
+                
+            case 7: // Switches A and B and C flipped
+                return AutonomousRoutineSelector.GetDriveStraightAndTurnAndReaproachAndShootCloseRouteRoutine();
+
+            default: // CANT READ
+                return AutonomousRoutineSelector.GetFillerRoutine();
         }
-
-        DashboardLogger.putString(Robot.ROBOT_STATE_LOG_KEY, "Disabled");
     }
 
     /**
-     * Initialization code for autonomous mode should go here.
-     * This code will be called each time the robot enters autonomous mode.
+     * Gets an autonomous routine that does nothing
+     * 
+     * @return very long WaitTask
      */
-    public void autonomousInit()
+    private static IControlTask GetFillerRoutine()
     {
-        // reset the position manager so that we consider ourself at the origin (0,0) and facing the 0 direction.
-        this.components.getPositionManager().reset();
-
-        // Create an autonomous driver
-        this.driver = new AutonomousDriver(this.components);
-
-        this.generalInit();
-
-        // log that we are in autonomous mode
-        DashboardLogger.putString(Robot.ROBOT_STATE_LOG_KEY, "Autonomous");
+        return new WaitTask(0);
     }
 
     /**
-     * Initialization code for teleop mode should go here.
-     * This code will be called each time the robot enters teleop mode.
+     * Gets an autonomous routine that moves the specified velocity for the specified time
+     * 
+     * @param time - time to drive forward
+     * @param xVelocity - velocity in the x to maintain while driving
+     * @param yVelocity - velocity in the y to maintain while driving
+     * @return DriveTimedTask of specified time, and x and y velocities
      */
-    public void teleopInit()
+    private static IControlTask GetDriveTimedRoutine(double time, double xVelocity, double yVelocity)
     {
-        // create driver for user's joystick
-        this.driver = new UserDriver(this.components);
-
-        this.generalInit();
-
-        // log that we are in teleop mode
-        DashboardLogger.putString(Robot.ROBOT_STATE_LOG_KEY, "Teleop");
+        return SequentialTask.Sequence(
+            new WaitTask(8.0),
+            ConcurrentTask.AllTasks(
+                new IntakeExtendTask(0.5, true),
+                new DriveTimedTask(time, xVelocity, yVelocity)));
     }
 
-    /**
-     * General initialization code for teleop/autonomous mode should go here.
-     */
-    public void generalInit()
+    private static IControlTask GetChevalDeFriseBreachRouteRoutine()
     {
-        // apply the driver to the controllers
-        this.controllers.setDriver(this.driver);
+        return SequentialTask.Sequence(
+            ConcurrentTask.AllTasks(
+                new IntakeExtendTask(TuningConstants.SHOOTER_LOWER_KICKER_DURATION, true),
+                new DriveRouteTask(
+                    (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 136.0 : 136.0,
+                    (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 136.0 : 136.0,
+                    2.0)),
+            new StingerTask(1.0, true),
+            ConcurrentTask.AllTasks(
+                new StingerTask(1.0, true),
+                new DriveRouteTask(
+                    (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 350.0 : 350.0,
+                    (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 350.0 : 350.0,
+                    5.0)));
     }
 
-    /**
-     * Periodic code for disabled mode should go here.
-     * This code will be called periodically at a regular rate while the robot is in disabled mode.
-     */
-    public void disabledPeriodic()
+    private static IControlTask GetPortcullisBreachRouteRoutine()
     {
+        return SequentialTask.Sequence(
+            ConcurrentTask.AllTasks(
+                new IntakeExtendTask(0.5, true),
+                new StingerTask(0.5, true)),
+            SequentialTask.Sequence(
+                ConcurrentTask.AllTasks(
+                    new DriveRouteTask(
+                        (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 250.0 : 250.0,
+                        (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 250.0 : 250.0,
+                        2.5),
+                    new StingerTask(2.5, true)),
+                new DriveRouteTask(
+                    (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 250.0 : 250.0,
+                    (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 250.0 : 250.0,
+                    2.5)),
+            new StingerTask(1.0, false));
     }
 
-    /**
-     * Periodic code for autonomous mode should go here.
-     * This code will be called periodically at a regular rate while the robot is in autonomous mode.
-     */
-    public void autonomousPeriodic()
+    private static IControlTask GetDriveStraightAndTurnAndShootCloseRouteRoutine()
     {
-        this.generalPeriodic();
+        return SequentialTask.Sequence(
+            ConcurrentTask.AllTasks(
+                new IntakeExtendTask(TuningConstants.SHOOTER_LOWER_KICKER_DURATION, true),
+                new DriveRouteTask(
+                    (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 585.0 : 585.0, //525.0
+                    (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 585.0 : 585.0, //525.0
+                    7.25)), // 7.0
+            ConcurrentTask.AllTasks(
+                SequentialTask.Sequence(
+                    new TurnTask(75.0, false), //68.0
+                    new WaitTask(0.5)),
+                new IntakeExtendTask(TuningConstants.SHOOTER_LOWER_KICKER_DURATION, false)),
+            new DriveRouteTask(
+                (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 400.0 : 400.0, //420.0
+                (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 400.0 : 400.0, //420.0
+                3.25), // 3.5
+            ConcurrentTask.AnyTasks(
+                SequentialTask.Sequence(
+                    new ShooterKickerTask(TuningConstants.SHOOTER_LOWER_KICKER_DURATION, true),
+                    new ShooterSpinUpTask(false, TuningConstants.SHOOTER_CLOSE_SHOT_VELOCITY, TuningConstants.SHOOTER_SPIN_UP_DURATION),
+                    new ShooterKickerTask(TuningConstants.SHOOTER_FIRE_DURATION, false),
+                    new ShooterSpinDownTask(TuningConstants.SHOOTER_REVERSE_DURATION)),
+                new PIDBrakeTask()));
     }
 
-    /**
-     * Periodic code for teleop mode should go here.
-     * This code will be called periodically at a regular rate while the robot is in teleop mode.
-     */
-    public void teleopPeriodic()
+    private static IControlTask GetDriveStraightAndTurnAndReaproachAndShootCloseRouteRoutine()
     {
-        this.generalPeriodic();
+        return SequentialTask.Sequence(
+            ConcurrentTask.AllTasks(
+                new IntakeExtendTask(TuningConstants.SHOOTER_LOWER_KICKER_DURATION, true),
+                new DriveRouteTask(
+                    (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 585.0 : 585.0, //525.0
+                    (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 585.0 : 585.0, //525.0
+                    7.0)),
+            ConcurrentTask.AllTasks(
+                SequentialTask.Sequence(
+                    new DriveRouteTask(
+                        (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 95.0 : 95.0, //420.0
+                        (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 0.0 : 0.0, //420.0
+                        .75),
+                    //new TurnTask(75.0, false), //68.0
+                    new WaitTask(0.3)),
+                new IntakeExtendTask(TuningConstants.SHOOTER_LOWER_KICKER_DURATION, false)),
+            new DriveRouteTask(
+                (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 400.0 : 400.0, //400
+                (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 400.0 : 400.0, //400
+                3.10),
+            new WaitTask(0.70),
+            new DriveRouteTask(
+                (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 100.0 : 100.0,
+                (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 100.0 : 100.0,
+                0.70),
+            ConcurrentTask.AnyTasks(
+                SequentialTask.Sequence(
+                    new ShooterKickerTask(TuningConstants.SHOOTER_LOWER_KICKER_DURATION, true),
+                    new ShooterSpinUpTask(false, TuningConstants.SHOOTER_CLOSE_SHOT_VELOCITY, TuningConstants.SHOOTER_SPIN_UP_DURATION),
+                    new ShooterKickerTask(TuningConstants.SHOOTER_FIRE_DURATION, false),
+                    new ShooterSpinDownTask(TuningConstants.SHOOTER_REVERSE_DURATION)),
+                new PIDBrakeTask()));
     }
 
-    /**
-     * General periodic code for teleop/autonomous mode should go here.
-     */
-    public void generalPeriodic()
+    private static IControlTask GetDriveStraightAndCurveAndShootCloseRouteRoutine()
     {
-        this.driver.update();
-
-        // run each controller
-        this.controllers.update();
+        return SequentialTask.Sequence(
+            ConcurrentTask.AllTasks(
+                new IntakeExtendTask(TuningConstants.SHOOTER_LOWER_KICKER_DURATION, true),
+                new DriveRouteTask(
+                    (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 175.0 : 175.0, //250.0
+                    (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 175.0 : 175.0, //250.0
+                    4.0)),
+            new DriveRouteTask(
+                (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 500.0 : 500.0, //490.0
+                (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 420.0 : 420.0, //420.0
+                4.0),
+            new DriveRouteTask(
+                (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 200.0 : 200.0, //200.0
+                (timeRatio) -> timeRatio < 0.9 ? timeRatio / 0.9 * 200.0 : 200.0, //200.0
+                3.0),
+            ConcurrentTask.AnyTasks(
+                SequentialTask.Sequence(
+                    new ShooterKickerTask(TuningConstants.SHOOTER_LOWER_KICKER_DURATION, true),
+                    new ShooterSpinUpTask(false, TuningConstants.SHOOTER_CLOSE_SHOT_VELOCITY, TuningConstants.SHOOTER_SPIN_UP_DURATION),
+                    new ShooterKickerTask(TuningConstants.SHOOTER_FIRE_DURATION, false),
+                    new ShooterSpinDownTask(TuningConstants.SHOOTER_REVERSE_DURATION)),
+                new PIDBrakeTask()));
     }
 }
-
 
 /*
 
